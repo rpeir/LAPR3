95667df841a38192b69f9205c010fbe73e10d307
Jose_Barbosa_1211359 <1211359@isep.ipp.pt>
2022-11-18 17:14:22 +0000

    Added graph related classes and interfaces

---
+++ esinf/src/esinf_sem3_pi/examples/map/Promotion.java
@@ -0,0 +1,11 @@
+package esinf_sem3_pi.examples.map;
+
+/**
+ *
+ *  @author DEI-ESINF
+ *
+ */
+public class Promotion {
+
+              
+}
---
+++ esinf/src/esinf_sem3_pi/examples/map/Trie.java
@@ -0,0 +1,17 @@
+package esinf_sem3_pi.examples.map;
+
+import esinf_sem3_pi.graph.Graph;
+import esinf_sem3_pi.graph.*;
+
+/**
+ *
+ * @author DEI-ESINF
+ *
+ */
+public class Trie {
+
+    public static Integer checkSequence(Graph<Integer, Character> trie, Character[] sequence) {
+
+         throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
