2452f52be76912e80fb2f8e5228bb8df384141f2
JoanaSofia <1211148@isep.ipp.pt>
2022-11-25 16:58:36 +0000

    added some domain classes

--- esinf/src/pi/sem3/Domain/Cabaz.java
+++ esinf/src/pi/sem3/Domain/Cabaz.java
@@ -8,10 +8,10 @@ public class Cabaz {
 
     private ArrayList<Produto> produtos;
 
-    public Cabaz(ClienteProdutorEmpresa clienteProdutorEmpresa, String diaDeProducao, Produto p1, Produto p2, Produto p3, Produto p4, Produto p5, Produto p6, Produto p7, Produto p8, Produto p9, Produto p10, Produto p11, Produto p12, Produto p13, Produto p14, Produto p15, Produto p16, Produto p17, Produto p18, Produto p19, Produto p20) {
+    public Cabaz(ClienteProdutorEmpresa clienteProdutorEmpresa, String diaDeProducao, ArrayList<Produto> produtos) {
         this.clienteProdutorEmpresa = clienteProdutorEmpresa;
         this.diaDeProducao = diaDeProducao;
-        this.produtos = new ArrayList<>();
+        this.produtos = produtos;
     }
 
     private ArrayList<Produto> getProdutos() {
