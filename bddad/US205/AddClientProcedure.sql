f2d9d540e62db7422d923cc8460f9983388afcfc
JoanaSofia <1211148@isep.ipp.pt>
2022-11-27 12:29:59 +0000

    working on us205

--- bddad/US205/AddClientProcedure.sql
+++ bddad/US205/AddClientProcedure.sql
@@ -64,21 +64,21 @@ begin
 end;
 /
 
---CREATE OR REPLACE PROCEDURE checkNrFiscal (nrFiscal IN Cliente.nrFiscal%type)
---is
---begin
---    if (nrFiscal is NULL) THEN
---        RAISE_APPLICATION_ERROR(-20000, 'nrFiscal não pode ser nulo!');
---    elsif (REGEXP_LIKE(nrFiscal, '[0-9]{9}')) THEN
---        RAISE_APPLICATION_ERROR(-20000, 'nrFiscal deve conter 9 digitos!');
---    end if;
---end;
---/
+CREATE OR REPLACE PROCEDURE checkNrFiscal (nrFiscal IN Cliente.nrFiscal%type)
+is
+begin
+    if (nrFiscal is NULL) then
+        RAISE_APPLICATION_ERROR(-20000, 'nrFiscal não pode ser nulo!');
+    elsif NOT (REGEXP_LIKE(nrFiscal, '[0-9]{9}')) then
+        RAISE_APPLICATION_ERROR(-20000, 'nrFiscal deve conter 9 digitos!');
+    end if;
+end;
+/
 
 CREATE OR REPLACE PROCEDURE checkEmail (email IN Cliente.email%type)
 is
 begin
-    if (email is NULL) THEN
+    if (email is NULL) then
         RAISE_APPLICATION_ERROR(-20000, 'email não pode ser nulo!');
     elsif (REGEXP_LIKE(email, '^[a-zA-Z0-9][a-zA-Z0-9._-]*@[a-zA-Z0-9][a-zA-Z0-9._-]*\\.[a-zA-Z]{2,4}$')) THEN
         RAISE_APPLICATION_ERROR(-20000, 'email invalido!');
@@ -91,9 +91,9 @@ CREATE OR REPLACE PROCEDURE checkPlafond (plafond IN Cliente.Plafond%TYPE)
 IS
 
 BEGIN
-    IF (plafond IS NULL) THEN
+    IF (plafond IS NULL) then
         RAISE_APPLICATION_ERROR(-20000, 'Plafond não pode ser nulo!');
-    ELSIF (plafond < 0) THEN
+    ELSIF (plafond < 0) then
         RAISE_APPLICATION_ERROR(-20000, 'Plafond inválido!');
     END IF;
 END;
@@ -104,14 +104,14 @@ CREATE OR REPLACE PROCEDURE checkNrFiscalJaExiste (c_nrFiscal IN Cliente.nrFisca
 IS
     cc_nrFiscal Cliente.nrFiscal%TYPE;
 
-BEGIN
+begin
     SELECT nrFiscal INTO cc_nrFiscal FROM Cliente c WHERE c.nrFiscal = c_nrFiscal;
 
     RAISE_APPLICATION_ERROR(-20000, 'nrFiscal ja existe!');
 EXCEPTION
-    WHEN NO_DATA_FOUND THEN
+    WHEN NO_DATA_FOUND then
         dbms_output.put_line('nrFiscal valido!');
-END;
+end;
 /
 
 CREATE OR REPLACE PROCEDURE checkEmailJaExiste (c_email IN Cliente.email%TYPE)
@@ -119,14 +119,14 @@ CREATE OR REPLACE PROCEDURE checkEmailJaExiste (c_email IN Cliente.email%TYPE)
 IS
     cc_email Cliente.email%TYPE;
 
-BEGIN
+begin
     SELECT email INTO cc_email FROM Cliente c WHERE c.email = c_email;
 
     RAISE_APPLICATION_ERROR(-20000, 'email ja existe!');
 EXCEPTION
     WHEN NO_DATA_FOUND THEN
         dbms_output.put_line('email valido!');
-END;
+end;
 /
 
 CREATE OR REPLACE FUNCTION criarCliente (
@@ -150,18 +150,18 @@ is
 
 begin
 
-    checkNome(c_nome);
-    checkEmail(c_email);
-    --checkNrFiscal(c_nrFiscal);
-    checkPlafond(c_plafond);
     checkNrFiscalJaExiste(c_nrFiscal);
     checkEmailJaExiste(c_email);
+    checkNome(c_nome);
+    checkEmail(c_email);
+    checkNrFiscal(c_nrFiscal);
+    checkPlafond(c_plafond);
     c_codCliente := adicionarNovoCliente(c_codInt, c_nome, c_nrFiscal, c_email, c_moradaCor, c_moradaEnt, c_plafond, u_email, u_password);
     dbms_output.put_line('Cliente inserido com id: ' || c_codCliente);
 return c_codCliente;
 
     EXCEPTION
-    WHEN others THEN
+    WHEN others then
         dbms_output.put_line(SQLERRM);
         RETURN NULL;
 
@@ -169,8 +169,8 @@ end;
 /
 
 DECLARE
-      l_success_code NUMBER;
-BEGIN
-      l_success_code := criarCliente(1, 'joao', 123456789, 'abcd1234@gmail.com', 'rua do isep', 'rua do isep dois', 1600, '1234abcd@gmail.com', 'abcd1234');
+      success_code NUMBER;
+begin
+      success_code := criarCliente(1, 'joao', 123456789, 'abcd1234@gmail.com', 'rua do isep', 'rua do isep dois', 1600, '1234abcd@gmail.com', 'abcd1234');
       rollback;
-END;
+end;
