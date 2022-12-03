34292f631001b891572ebc22a13b83d1dd289cad
JoanaSofia <1211148@isep.ipp.pt>
2022-12-03 18:29:11 +0000

    working on us207

--- bddad/US207/rentabilidade.sql
+++ bddad/US207/rentabilidade.sql
@@ -1,24 +1,33 @@
-create or replace PROCEDURE rentabilidade( colheitaA SetoresAgricolas_Colheitas_Culturas.codColheita%type) AS
- c_Setor SetoresAgricolas_Colheitas_Culturas.codSetorAgricola%type;
- c_cultura SetoresAgricolas_Colheitas_Culturas.codCultura%type;
+create or replace PROCEDURE rentabilidade( colheitaA SetoresAgricolas_Colheitas_Culturas.codColheita%type)
+
+AS
+ c_codSetorAgricola SetoresAgricolas_Colheitas_Culturas.codSetorAgricola%type;
+ c_codCultura SetoresAgricolas_Colheitas_Culturas.codCultura%type;
  c_valorSetor SetoresAgricolas_Colheitas_Culturas.valorSetor%type;
 
-	CURSOR c_culturas(colheita SetoresAgricolas_Colheitas_Culturas.codColheita%type) IS
+	CURSOR c_culturas
+	(colheita SetoresAgricolas_Colheitas_Culturas.codColheita%type) IS
 		SELECT codSetorAgricola, codCultura, valorSetor from SetoresAgricolas_Colheitas_Culturas
 		 WHERE codColheita = colheita
 		 ORDER BY valorSetor DESC;
 	colheita_inexistente EXCEPTION;
 	dummy Colheitas.codColheita%type;
+
+
+
+
 BEGIN
-	SELECT COUNT(*) INTO dummy FROM Colheitas WHERE codColheita = colheita;
+	SELECT COUNT(*) INTO dummy FROM Colheitas WHERE codColheita = colheitaA;
 	IF dummy = 0 THEN
 	 RAISE colheita_inexistente;
 	ELSE
-	 OPEN c_colheitas(colheitaA);
+	 OPEN c_culturas(colheitaA);
 	LOOP
 		FETCH c_culturas INTO c_codSetorAgricola, c_codCultura, c_valorSetor;
-		EXIT WHEN c_colheitas%notfound
-			dbms_output.put_line(codSetorAgricola || "-" ||  codCultura || "-" || valorSetor);
-	END LOOP
+		EXIT WHEN c_culturas%notfound;
+			dbms_output.put_line(c_codSetorAgricola || '-' ||  c_codCultura || '-' || c_valorSetor);
+	END LOOP;
 	CLOSE c_culturas;
+	end if;
 END;
+/
