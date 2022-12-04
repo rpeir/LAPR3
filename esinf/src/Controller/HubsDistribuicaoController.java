3d9ecce497a48737cfc567ca32e76bf9d17ca0e9
JoanaSofia <1211148@isep.ipp.pt>
2022-12-04 12:19:51 +0000

    working on us303

--- esinf/src/Controller/HubsDistribuicaoController.java
+++ esinf/src/Controller/HubsDistribuicaoController.java
@@ -30,7 +30,7 @@ public class HubsDistribuicaoController {
     }
 
     public Map<ClienteProdutorEmpresa, Double> getMediaDistancia(Graph<Localizacao, Integer> grafo, int n) {
-        if(n > getEmpresas().size()) System.out.println("O numero de hubs nao pode ser menor que o numero de empresas");
+        if(n > getEmpresas().size() || n < 1) System.out.println("O numero de hubs inserido e invalido");
         else {
         Map<ClienteProdutorEmpresa, Double> medias = new HashMap<>();
         List<ClienteProdutorEmpresa> listaEmpresas = getEmpresas();
