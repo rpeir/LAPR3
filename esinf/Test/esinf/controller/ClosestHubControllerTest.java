5b7d49ee456f5d4350094f70a2d9451610885094
Ines <1210814@isep.ipp.pt>
2022-12-04 18:16:45 +0000

    [ESINF] corrections regarding package and class names in tests

---
+++ esinf/Test/esinf/controller/ClosestHubControllerTest.java
@@ -0,0 +1,17 @@
+package esinf.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ClosestHubControllerTest {
+
+    @Test
+    public void getClosestHub() {
+
+    }
+
+    @Test
+    public void getAllClosestHubs() {
+    }
+}
