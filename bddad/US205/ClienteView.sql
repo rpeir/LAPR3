4eb0eda0906e7662c4e918808b2ca7bd4a6b327a
JoanaSofia <1211148@isep.ipp.pt>
2022-12-25 21:40:10 +0000

    fixing us205

--- bddad/US205/ClienteView.sql
+++ bddad/US205/ClienteView.sql
@@ -1,22 +1,27 @@
 CREATE VIEW ClienteTotalVendasView AS SELECT codInterno "Codigo Interno", (
     SELECT COUNT(codInterno) FROM (SELECT Pedidos_Pagamentos_Entregas.codPedido, Pedidos_Pagamentos_Entregas.estadoAtual, Pedidos.codInterno, Pedidos.dataPedido FROM Pedidos INNER JOIN Pedidos_Pagamentos_Entregas ON Pedidos.codPedido = Pedidos_Pagamentos_Entregas.codPedido)
     WHERE codInterno = c.codInterno
-    AND estadoAtual = 'Pago'
+    AND estadoAtual = 'P'
     AND dataPedido BETWEEN SYSDATE AND add_months(SYSDATE, -12)
 ) AS "Total vendas nos ultimos 12 meses"
 FROM Clientes c;
 
-CREATE VIEW DataUltimoIncidente AS SELECT codInterno "Codigo Interno", (
-    SELECT dataOcorrencia
-    FROM Incidentes
-    WHERE codIncidente=(SELECT max(codIncidente) FROM Incidentes)
-) AS "Data do ultimo incidente"
+CREATE VIEW DataUltimoIncidente AS
+SELECT codInterno "Codigo Interno",
+       (CASE WHEN (SELECT max(TO_CHAR(dataOcorrencia, 'DD-MM-YYYY'))
+                  FROM incidentes i
+                  WHERE i.codInterno = c.codinterno) IS NULL
+             THEN 'Sem incidentes à data'
+             ELSE (SELECT max(TO_CHAR(dataOcorrencia, 'DD-MM-YYYY'))
+                   FROM incidentes i
+                   WHERE i.codInterno = c.codinterno)
+        END) AS "Data do ultimo incidente"
 FROM Clientes c;
 
 CREATE VIEW EncomendasEntreguesNaoPagas AS SELECT codInterno "Codigo Interno", (
-    SELECT COUNT(codInterno) FROM Pedidos_Pagamentos_Entregas
+    SELECT COUNT(codInterno) FROM (SELECT Pedidos_Pagamentos_Entregas.codPedido, Pedidos_Pagamentos_Entregas.codPagamento, Pedidos_Pagamentos_Entregas.estadoAtual, Pedidos.codInterno, Pedidos.dataPedido FROM Pedidos INNER JOIN Pedidos_Pagamentos_Entregas ON Pedidos.codPedido = Pedidos_Pagamentos_Entregas.codPedido)
     WHERE codInterno = c.codInterno
-    AND estadoAtual = 'Entregue'
+    AND estadoAtual = 'E'
     AND codPagamento IS NULL
 ) AS "Encomendas entregues mas nao pagas"
 FROM Clientes c;
