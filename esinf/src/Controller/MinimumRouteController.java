bdaa3c7c21d477a1fdfeaf1cc710c4c85fd0380e
JoanaSofia <1211148@isep.ipp.pt>
2022-12-28 19:15:18 +0000

    added methods and store classes.

--- esinf/src/Controller/MinimumRouteController.java
+++ esinf/src/Controller/MinimumRouteController.java
@@ -28,37 +28,6 @@ public class MinimumRouteController {
     }
 
     public void getMinimumRoute(int dia) {
-        // lista de expedicao para o dia pretendido
-        ListaExpedicoes expedicao = listaExpedicoesStore.getExpedicao(dia);
-        // lista de cabazes para o dia pretendido
-        List<Cabaz> cabazes = expedicao.get_listaExpedicoes();
-        System.out.println("Cabazes para o dia " + dia + ":\n");
-        for (Cabaz cabaz : cabazes) {
-
-            System.out.println(cabaz.getCliente() + " -> " + cabaz.getProdutos());
-        }
-        // lista dos clientes para o dia pretendido
-        List<ClienteProdutorEmpresa> clientes = new ArrayList<>();
-        for (Cabaz cabaz : cabazes) {
-            if (cpeStore.containsCPE(cabaz.getCliente())) {
-                clientes.add(cpeStore.getCPE(cabaz.getCliente()));
-            }
-        }
-        System.out.println("Clientes para o dia " + dia + ":\n");
-        System.out.println(Arrays.toString(clientes.toArray()));
-        // lista com o hub mais proximo de cada cliente
-        List<ClienteProdutorEmpresa> hubsCliente = new ArrayList<>();
-        for (ClienteProdutorEmpresa cliente : clientes) {
-            hubsCliente.add(ctrl.getClosestHub(cliente));
-        }
-        System.out.println("Hubs mais proximos dos clientes para o dia " + dia + ":\n");
-        System.out.println(Arrays.toString(hubsCliente.toArray()));
-
-        // grafo geral
-        Graph<Localizacao, Integer> grafo = app.getGraph();
-        // numero de hubs
-        int n = clientes.size();
-        int distanciaTotal = 0;
 
     }
 }
