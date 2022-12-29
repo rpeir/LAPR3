4901a0fcdcce364a65650801c5c5ddbdb3173f26
Ines <1210814@isep.ipp.pt>
2022-12-29 11:50:28 +0000

    [ARQCP] implemented ability to write sensor to csv

---
+++ arqcp/US112/write_sensor_file.h
@@ -0,0 +1,16 @@
+#ifndef EX08_H
+#define EX08_H
+typedef struct
+{
+    unsigned short id;
+    unsigned char sensor_type;
+    unsigned short max_limit; // limites do sensor
+    unsigned short min_limit;
+    unsigned long frequency; // frequency de leituras (em segundos)
+    unsigned long readings_size; // tamanho do array de leituras
+    unsigned short *readings; // array de leituras diárias
+} Sensor;
+
+void write_sensor_file(Sensor *sensor);
+
+#endif
