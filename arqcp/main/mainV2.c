0b6286914848ee8bdc171d0dfb14e396910c1ba1
JoanaSofia <1211148@isep.ipp.pt>
2022-12-31 13:18:39 +0000

    us110 (incomplete)

--- arqcp/main/mainV2.c
+++ arqcp/main/mainV2.c
@@ -5,18 +5,16 @@
 //////////////////////////////////////////////////////////////////////////////////
 #define INITIAL_ARRAY_SIZE 10
 
-Sensor* sensors = malloc(INITIAL_ARRAY_SIZE * sizeof(Sensor)); // Allocate initial memory for the array
-int num_sensors = 0; // Keep track of the number of sensors in the array
-int array_size = INITIAL_ARRAY_SIZE; // Keep track of the size of the array
+Sensor* sensors = malloc(INITIAL_ARRAY_SIZE * sizeof(Sensor));
+int num_sensors = 0;
+int array_size = INITIAL_ARRAY_SIZE;
 
 void add_sensor_to_list(Sensor sensor) {
-    // If the array is full, reallocate memory for the array to make it larger
     if (num_sensors == array_size) {
     array_size *= 2;
     sensors = realloc(sensors, array_size * sizeof(Sensor));
     }
 
-    // Add the sensor to the end of the array
     sensors[num_sensors] = sensor;
     num_sensors++;
 }
@@ -34,40 +32,34 @@ typedef struct
     ... // adicionar o que acharem conveniente
 } Sensor;
 
-// Parses a line from the CSV file and stores the data in a Sensor structure
+
 void parse_sensor_line(char* line, Sensor* sensor) {
     char* token;
-    char* line_copy = strdup(line); // Make a copy of the line to tokenize
-
-    // Extract the ID field
+    char* line_copy = strdup(line);
+    // atoi = ascii to integer
     token = strtok(line_copy, ",");
     sensor->id = (unsigned short)atoi(token);
 
-    // Extract the sensor type field
     token = strtok(NULL, ",");
     sensor->sensor_type = (unsigned char)token[0];
 
-    // Extract the max limit field
     token = strtok(NULL, ",");
     sensor->max_limit = (unsigned short)atoi(token);
 
-    // Extract the min limit field
     token = strtok(NULL, ",");
     sensor->min_limit = (unsigned short)atoi(token);
 
-    // Extract the frequency field
     token = strtok(NULL, ",");
     sensor->frequency = (unsigned long)atoi(token);
 
     // Calculate the size of the readings array
     sensor->readings_size = 86400 / sensor->frequency; // 86400 seconds in a day
 
-    // Allocate memory for the readings array
     sensor->readings = malloc(sensor->readings_size * sizeof(unsigned short));
 
     //fill readings array here
 
-    free(line_copy); // Free the copy of the line
+    free(line_copy);
 }
 ///////////////////////////////////////////////////////////////////////////////////
 int main() {
@@ -75,26 +67,22 @@ int main() {
     int choice;
     printf("1 - Import file\n2 - Input info\n");
     scanf("%d", &choice);
-    if(choice==1) {
 
-    // Open the CSV file
+    if(choice==1) {
     FILE* file = fopen("sensores.csv", "r");
     if (file == NULL) {
     perror("Error opening file");
     return 1;
     }
 
-    // Read the file line by line
     char line[1024];
     while (fgets(line, 1024, file) != NULL) {
     Sensor sensor;
     parse_sensor_line(line, &sensor);
 
-    // Add the sensor to a list or array of sensors
     add_sensor_to_list(sensor);
     }
 
-    // Close the file
     fclose(file);
 
     }
