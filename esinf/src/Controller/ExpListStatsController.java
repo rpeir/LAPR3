c93737d4534d43b666358d107b277d52f86e6bf8
rpeir <1211345@isep.ipp.pt>
2023-01-07 17:59:46 +0000

    [ESINF] Added getProdutos(), satisfiedCabaz() and getDiffProdutores() methods on Cabaz class; Remade getStatsByCliente() and getStatsByCabaz() methods and added its new inner methods;

--- esinf/src/Controller/ExpListStatsController.java
+++ esinf/src/Controller/ExpListStatsController.java
@@ -10,6 +10,15 @@ import java.util.*;
 
 public class ExpListStatsController {
 
+    static final String statNameCTS = "Nº de cabazes totalmente satisfeitos"; // Nome da estatística "Cabazes totalmente satisfeitos"
+    static final String statNameCPS = "Nº de cabazes parcialmente satisfeitos"; // Nome da estatística "Cabazes parcialmente satisfeitos"
+    static final String statNamePFC = "Nº de produtores distintos que forneceram cabazes"; // Nome da estatística "Produtores distintos que forneceram cabazes"
+    static final String statNamePTS = "Nº de produtos totalmente satisfeitos"; // Nome da estatística "Produtos totalmente satisfeitos"
+    static final String statNamePPS = "Nº de produtos parcialmente satisfeitos"; // Nome da estatística "Produtos parcialmente satisfeitos"
+    static final String statNamePNS = "Nº de produtos nao satisfeitos"; // Nome da estatística "Produtos nao satisfeitos"
+    static final String statNamePer = "Percentagem total do cabaz satisfeito"; // Nome da estatística "Percentagem total do cabaz satisfeito"
+    static final String statNamePFOC = "Nº de produtores distintos que forneceram o cabaz"; // Nome da estatística "Produtores distintos que forneceram o cabaz"
+
     private final Map<String, ClienteProdutorEmpresa> mapCPE;
     private final Map<Integer, Map<ClienteProdutorEmpresa, Cabaz>> expedicoes;
 
@@ -24,71 +33,96 @@ public class ExpListStatsController {
     }
 
     /**
+     * Create a list of statistics for each client in all expeditions
+     * @return List of statistics for each client
+     */
+    public List<ListStatistics> getStatsByCliente() {
+        Map<String, List<Integer>> clientesStatsValues = new HashMap<>(); // posicao : 0 - PTS, 1 - PPS
+        Map<String, Set<String>> clientesDiffProdutores = new HashMap<>(); // produtores distintos que forneceram cabazes
+
+        for (Map.Entry<Integer, Map<ClienteProdutorEmpresa, Cabaz>> diaEntry: expedicoes.entrySet()) { // para cada dia
+            int dia = diaEntry.getKey();
+            Map<ClienteProdutorEmpresa, Cabaz> expedicao = diaEntry.getValue();
+
+            fillClientesStatsForExpedicao(clientesStatsValues, clientesDiffProdutores, expedicao, dia);
+        }
+
+        return createListStatisticsForEachCliente(clientesStatsValues, clientesDiffProdutores);
+    }
+
+    /**
      * Create a list of statistics for each client for the given day
      * @param dia day of the expeditions
      * @return List of statistics for each client
      */
     public List<ListStatistics> getStatsByCliente(int dia) {
-        /*if (!expedicoes.containsKey(dia)) throw new IllegalArgumentException("Não existe lista de expedicoes para o dia " + dia);
+        if (!expedicoes.containsKey(dia)) throw new IllegalArgumentException("Não existe lista de expedicoes para o dia " + dia);
 
-        HashMap<String, ListStatistics> finalStats = new HashMap<>(); // lista final para ser retornada
-        ListaExpedicoes listaExpedicoes = expedicoes.get(dia);
+        Map<String, List<Integer>> clientesStatsValues = new HashMap<>(); // posicao : 0 - PTS, 1 - PPS
+        Map<String, Set<String>> clientesDiffProdutores = new HashMap<>(); // produtores distintos que forneceram cabazes
 
-        createListStatisticsForEachClienteProdutor(finalStats,listaExpedicoes); // cria uma lista de estatisticas para cada cpe e adiciona ao hashmap finalStats
+        fillClientesStatsForExpedicao(clientesStatsValues, clientesDiffProdutores, expedicoes.get(dia), dia);
 
-        final String statNameCTS = "Nº de cabazes totalmente satisfeitos"; // Nome da estatística "Cabazes totalmente satisfeitos"
-        final String statNameCPS = "Nº de cabazes parcialmente satisfeitos"; // Nome da estatística "Cabazes parcialmente satisfeitos"
-        final String statNamePFC = "Nº de produtores distintos que forneceram cabazes"; // Nome da estatística "Produtores distintos que forneceram cabazes"
+        return createListStatisticsForEachCliente(clientesStatsValues, clientesDiffProdutores);
+    }
 
-        List<Cabaz> sorted = listaExpedicoes.get_listaExpedicoes();
-        // Ordena a lista de expedicoes por clienteProdutorEmpresa para manipular o mesmo em cada iteracao
-        sorted.sort(Comparator.comparing(Pedido::getClienteProdutor));
-
-        String oldCP = sorted.get(0).getClienteProdutor();
-        int statValueCTS = 0;
-        int statValueCPS = 0;
-        HashSet<String> fornecedores = new HashSet<>();
-        for (Cabaz cabaz : sorted) {
-            String clienteProdutor = cabaz.getClienteProdutor();
-
-            if (!oldCP.equals(clienteProdutor)) { // Se o cpe for diferente do anterior
-                ListStatistics stats = finalStats.get(oldCP);
-                stats.addStat(statNameCTS, statValueCTS); // Cria a estatistica com o valor de cabazes totalmente satisfeitos
-                stats.addStat(statNameCPS, statValueCPS); // Cria a estatistica com o valor de cabazes parcialmente satisfeitos
-                stats.addStat(statNamePFC, fornecedores.size()); // Cria a estatistica com o valor de produtores distintos que forneceram cabazes
-                statValueCTS = 0; // Reseta o valor para o novo cpe
-                statValueCPS = 0; // Reseta o valor para o novo cpe
-                fornecedores.clear(); // Reseta o valor para o novo cpe
+    private void fillClientesStatsForExpedicao(Map<String, List<Integer>> clientesStatsValues, Map<String, Set<String>> clientesDiffProdutores, Map<ClienteProdutorEmpresa, Cabaz> expedicao, int dia) {
+        for (Map.Entry<ClienteProdutorEmpresa, Cabaz> cliente : expedicao.entrySet()) { // para cada cliente
+            ClienteProdutorEmpresa cpe = cliente.getKey();
+            Cabaz cabaz = cliente.getValue();
+            String clienteID = cpe.getId();
+            if (!clientesStatsValues.containsKey(clienteID)) { // se ainda não existe o cliente na lista de clientes
+                clientesStatsValues.put(clienteID, new ArrayList<>(Arrays.asList(0, 0)));
+                clientesDiffProdutores.put(clienteID, new HashSet<>());
             }
+            List<Integer> statsValues = clientesStatsValues.get(clienteID);
+            clientesDiffProdutores.get(clienteID).addAll(cabaz.getDiffProdutores());
 
-            Pedido pedido = mapCPE.get(clienteProdutor).getCabaz(cabaz.getDiaDeProducao());
-            if (cabaz.isSatisfied(pedido)) statValueCTS++; // Se o cabaz estiver totalmente satisfeito aumenta o valor
-            else if (cabaz.isPartillySatisfied(pedido)) statValueCPS++; // Se o cabaz estiver parcialmente satisfeito aumenta o valor
-            fornecedores.addAll(cabaz.getProdutores()); // Adiciona os fornecedores do cabaz ao HashSet
-            oldCP = clienteProdutor;
-
+            Pedido pedido = cpe.getCabaz(dia); // obter o pedido do cliente
+            int satisfacao = cabaz.satisfiedCabaz(pedido.getProdutos()); // obter a satisfacao do cabaz
+            if (satisfacao == 1) { // cabaz totalmente satisfeito
+                statsValues.set(0, statsValues.get(0) + 1);
+            } else if (satisfacao == 0) { // cabaz parcialmente satisfeito
+                statsValues.set(1, statsValues.get(1) + 1);
+            }
         }
-
-        ListStatistics stats = finalStats.get(oldCP);
-        stats.addStat(statNameCTS, statValueCTS); // Cria a estatistica com o valor de cabazes totalmente satisfeitos para o ultimo cpe
-        stats.addStat(statNameCPS, statValueCPS); // Cria a estatistica com o valor de cabazes parcialmente satisfeitos para o ultimo cpe
-        stats.addStat(statNamePFC, fornecedores.size()); // Cria a estatistica com o valor de produtores distintos que forneceram cabazes para o ultimo cpe
-
-        return new ArrayList<>(finalStats.values());*/
-        return null;
     }
 
     /**
-     * Create a ListStatistics for each ClienteProdutorEmpresa in ListaExpedicoes and add it to the stats HashMap
-     * @param stats HashMap to add the ListStatistics
+     * Creates a List of ListStatistics for each client
+     * @param clientesStatsValues Map with the values of the statistics for each client
+     * @param clientesDiffProdutores Map with the different producers that provided a cabaz for each client
+     * @return List of statistics for each client
      */
-    private void createListStatisticsForEachClienteProdutor(HashMap<String, ListStatistics> stats) {
-        /*for (Cabaz cabaz : listaExpedicoes.get_listaExpedicoes()) {
-            String clienteProdutor = cabaz.getClienteProdutor();
-            if (!stats.containsKey(clienteProdutor)) {
-                stats.put(clienteProdutor, new ListStatistics(clienteProdutor));
-            }
-        }*/
+    private List<ListStatistics> createListStatisticsForEachCliente(Map<String, List<Integer>> clientesStatsValues, Map<String, Set<String>> clientesDiffProdutores) {
+        List<ListStatistics> finalStats = new ArrayList<>(); // Lista de estatísticas a retornar
+
+        for (Map.Entry<String, List<Integer>> entry : clientesStatsValues.entrySet()) { // Para cada cliente
+            String clienteID = entry.getKey();
+            List<Integer> statsValues = entry.getValue();
+            ListStatistics listStatistics = new ListStatistics(clienteID); // Cria nova lista de estatísticas
+            listStatistics.addStat(statNameCTS, statsValues.get(0)); // Adiciona a estatística "Cabazes totalmente satisfeitos"
+            listStatistics.addStat(statNameCPS, statsValues.get(1)); // Adiciona a estatística "Cabazes parcialmente satisfeitos"
+            listStatistics.addStat(statNamePFC, clientesDiffProdutores.get(clienteID).size()); // Adiciona a estatística "Produtores distintos que forneceram cabazes"
+            finalStats.add(listStatistics);
+        }
+
+        return finalStats;
+    }
+
+    /**
+     * Create a list of statistics for each cabaz in all expeditions
+     * @return List of statistics for each cabaz
+     */
+    public List<ListStatistics> getStatsByCabaz() {
+        List<ListStatistics> finalStats = new ArrayList<>(); // Lista de estatísticas a retornar
+        for (Map.Entry<Integer, Map<ClienteProdutorEmpresa, Cabaz>> diaEntry: expedicoes.entrySet()) { // para cada dia
+            int dia = diaEntry.getKey();
+            Map<ClienteProdutorEmpresa, Cabaz> expedicao = diaEntry.getValue();
+
+            finalStats.addAll(createListStatisticsForEachCabaz(expedicao, dia));
+        }
+        return finalStats;
     }
 
     /**
@@ -97,21 +131,28 @@ public class ExpListStatsController {
      * @return List of statistics for each cabaz
      */
     public List<ListStatistics> getStatsByCabaz(int dia) {
-        /*if (!expedicoes.containsKey(dia)) throw new IllegalArgumentException("Não existe lista de expedicoes para o dia " + dia);
+        if (!expedicoes.containsKey(dia)) throw new IllegalArgumentException("Não existe lista de expedicoes para o dia " + dia);
 
+        Map<ClienteProdutorEmpresa, Cabaz> expedicao = expedicoes.get(dia);
+
+        return createListStatisticsForEachCabaz(expedicao, dia);
+    }
+
+    /**
+     * Create a list of statistics for each cabaz in all expeditions
+     * @param dia day of the expeditions
+     * @param expedicao Map with the cabazes for the given day
+     * @return List of statistics for each cabaz
+     */
+    private List<ListStatistics> createListStatisticsForEachCabaz(Map<ClienteProdutorEmpresa, Cabaz> expedicao, int dia) {
         HashMap<String, ListStatistics> finalStats = new HashMap<>(); // lista final para ser retornada
-        ListaExpedicoes listaExpedicoes = expedicoes.get(dia);
 
-        final String statNamePTS = "Nº de produtos totalmente satisfeitos"; // Nome da estatística "Produtos totalmente satisfeitos"
-        final String statNamePPS = "Nº de produtos parcialmente satisfeitos"; // Nome da estatística "Produtos parcialmente satisfeitos"
-        final String statNamePNS = "Nº de produtos nao satisfeitos"; // Nome da estatística "Produtos nao satisfeitos"
-        final String statNamePer = "Percentagem total do cabaz satisfeito"; // Nome da estatística "Percentagem total do cabaz satisfeito"
-        final String statNamePFC = "Nº de produtores distintos que forneceram o cabaz"; // Nome da estatística "Produtores distintos que forneceram o cabaz"
-
-        for (Cabaz cabaz : listaExpedicoes.get_listaExpedicoes()) {
-            ListStatistics stats = new ListStatistics(String.format("Cabaz de %s", cabaz.getClienteProdutor()));
+        for (Map.Entry<ClienteProdutorEmpresa, Cabaz> entry : expedicao.entrySet()) { // para cada cabaz
+            ClienteProdutorEmpresa cliente = entry.getKey();
+            Cabaz cabaz = entry.getValue();
+            ListStatistics stats = new ListStatistics(String.format("Cabaz de %s", cliente.getId())); // cria nova lista de estatísticas
             List<Float> produtosCabaz = cabaz.getProdutos();
-            List<Float> produtosPedido = mapCPE.get(cabaz.getClienteProdutor()).getCabaz(dia).getProdutos();
+            List<Float> produtosPedido = mapCPE.get(cliente.getId()).getCabaz(dia).getProdutos();
 
             int pts = 0; // Nº de produtos totalmente satisfeitos
             int pps = 0; // Nº de produtos parcialmente satisfeitos
@@ -137,12 +178,11 @@ public class ExpListStatsController {
             stats.addStat(statNamePPS, pps); // Cria a estatistica com o valor de produtos parcialmente satisfeitos
             stats.addStat(statNamePNS, pns); // Cria a estatistica com o valor de produtos nao satisfeitos
             stats.addStat(statNamePer, String.format("%.2f %%", ((float) qtSatisfeita/qtPedida) * 100)) ; // Cria a estatistica com o valor da percentagem total do cabaz satisfeito
-            stats.addStat(statNamePFC, new HashSet<>(cabaz.getProdutores()).size()); // Cria a estatistica com o valor de produtores distintos que forneceram o cabaz
-            finalStats.put(cabaz.getClienteProdutor(), stats);
+            stats.addStat(statNamePFOC, cabaz.getDiffProdutores().size()); // Cria a estatistica com o valor de produtores distintos que forneceram o cabaz
+            finalStats.put(cabaz.getCliente(), stats);
         }
 
-        return new ArrayList<>(finalStats.values());*/
-        return null;
+        return new ArrayList<>(finalStats.values());
     }
 
     /**
