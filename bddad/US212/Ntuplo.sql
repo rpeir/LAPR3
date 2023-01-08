0f56f8a4fe48838daad5575e255904f6d205373f
Delcio Monjane <1211739@isep.ipp.pt>
2023-01-08 16:48:28 +0000

    us215 and some fixes in us212

--- bddad/US212/Ntuplo.sql
+++ bddad/US212/Ntuplo.sql
@@ -3,7 +3,7 @@ is
 string_return varchar(25);
 begin
 Select input_string into string_return from (
-                                                SELECT ROW_NUMBER() OVER (ORDER BY input_string)
+                                                SELECT ROW_NUMBER() OVER (ORDER BY 1)
 		 AS row_num, input_string FROM input_sensor)
 WHERE row_num = parm_n;
 return (string_return);
--- bddad/US212/validateCodTipoSensor.sql
+++ bddad/US212/validateCodTipoSensor.sql
@@ -1,9 +1,9 @@
-create or replace function validateCodTipoSensor(codTipoSensor TiposSensores.codTipoSensor%type) return int
+create or replace function validateCodTipoSensor(v_codTipoSensor TiposSensores.codTipoSensor%type) return int
 as
     resultado int;
     begin
         Select count(*) into resultado from  TiposSensores tp
-            where tp.codTipoSensor like 'HS';
+            where tp.codTipoSensor like v_codTipoSensor;
 
         return resultado;
     end;
